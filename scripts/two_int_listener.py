1b606452cfd786ab8d204d0c6a758c5c6c7d6431
Deepana Ishtaweera <33963319+deepanaishtaweera@users.noreply.github.com>
2023-01-25 16:38:29 +0530

    Update script after validation

--- scripts/two_int_listener.py
+++ scripts/two_int_listener.py
@@ -1,43 +1,49 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python
 import rospy
 
 from std_msgs.msg import Int16
 
 # START CODE HERE #
-# Import the created msg file
+# TODO: Import the created msg file
 
 # END CODE HERE #
 
+
 def callback(data):
-	# START CODE HERE #
+    # START CODE HERE #
+    # TODO: Complete callback function
+    global pub
+
     total = None
-    rospy.loginfo(None + " + " + None + " = " + str(total.data)+"\n")
-	pub.publish(total)
+    rospy.loginfo(None + " + " + None +
+                  " = " + str(total.data)+"\n")
+    pub.publish(total)
     # END CODE HERE #
 
 
 def talker_listener():
-	
-    # START CODE HERE #
-    # Create a ros node named 'two_ints_listner_node'
 
+    # START CODE HERE #
+    # TODO: Create a ros node named 'two_ints_listner_node'
+    
     # END CODE HERE #
-	
 
-	global pub
+    global pub
 
     # START CODE HERE #
+    # TODO: 
     # 1. Define the publisher 'pub' to publish '/sum' topic
     # 2. Call the rospy.Subscriber() with topic, dtype and callback
     pub = None
-    
+
+
     # END CODE HERE #
-	
-	rospy.spin()
+
+    rospy.spin()
 
 
 if __name__ == '__main__':
-	try:
-		talker_listener()
-	except rospy.ROSInterruptException:
-		raise e
+    try:
+        talker_listener()
+    except rospy.ROSInterruptException as e:
+        raise e
